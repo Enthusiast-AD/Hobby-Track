3d6c8efd89dbeb70ae74e31f893139e3c856ccd4
Ansh Deep <ad0811@srmist.edu.in>
2025-12-27 16:49:50 +0530

    add models

---
+++ backend/src/models/user.js
@@ -0,0 +1,10 @@
+import mongoose, {Schema} from 'mongoose';
+
+
+const userSchema = new Schema({
+    username: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    currentMotive: {type: String, default: ''},
+}, {timestamps: true});
+
+export const User = mongoose.model('User', userSchema);
