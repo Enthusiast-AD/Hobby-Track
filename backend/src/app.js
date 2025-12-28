0ca589c2395bb8ea2353a8c213d0d77ee04efc34
Ansh Deep <ad0811@srmist.edu.in>
2025-12-28 16:17:51 +0530

    add cors origin and setup index

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true // Allow cookies to be sent with requests
+}))
 
 app.use(express.json());
 
@@ -10,4 +16,4 @@ import activityRoutes from './routes/activity.routes.js';
 // Using routes
 app.use('/activity', activityRoutes);
 
-export default app;
+export { app };
