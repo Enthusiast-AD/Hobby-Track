ec151ffd6b2ccf38dd3b281c5610e92579ca23bb
Ansh Deep <ad0811@srmist.edu.in>
2025-12-29 17:06:42 +0530

    feat: add activity routes

--- backend/src/controllers/activity.controllers.js
+++ backend/src/controllers/activity.controllers.js
@@ -39,4 +39,21 @@ const getUserStats = asyncHandler(async (req, res) => {
     return res.status(200).json(new ApiResponse(200, stats, "User activity stats retrieved successfully"))
 });
 
-export { getUserStats };
+const createActivityLog = asyncHandler(async (req, res) => {
+    const { userId, habitId } = req.body;
+    
+    if(!isValidObjectId(userId)){
+        throw new ApiError(400, "Invalid user ID");
+    }
+
+    const newLog  = await ActivityLog.create({
+        userId,
+        habitId,
+        date: new Date(),
+        count: 1
+    });
+
+    return res.status(201).json(new ApiResponse(201, newLog, "Activity log created successfully"));
+});
+
+export { getUserStats, createActivityLog };
