0f6359058201bd8a6fe54ad4f700f968a2d21930
Ansh Deep <ad0811@srmist.edu.in>
2025-12-31 15:37:38 +0530

    feat: add user routes and error handling middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,19 +5,30 @@ const app = express();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN || '*',
-    credentials: true // Allow cookies to be sent with requests
+    credentials: true
 }))
 
 app.use(express.json());
 
-// Importing routes
 import activityRoutes from './routes/activity.routes.js';
 import authRoutes from './routes/auth.routes.js';
 import habitRoutes from './routes/habit.routes.js';
+import userRoutes from './routes/user.routes.js'
 
-// Using routes
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/activity', activityRoutes);
 app.use('/api/v1/habits', habitRoutes);
+app.use('/api/v1/users', userRoutes);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    
+    return res.status(statusCode).json({
+        success: false,
+        message: message,
+        errors: err.errors || []
+    });
+});
 
 export { app };
