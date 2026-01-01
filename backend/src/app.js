4ba72404f4a5620d026f91a3a964bded9e2dcc4c
Ansh Deep <ad0811@srmist.edu.in>
2026-01-01 17:09:01 +0530

    feat: Refactor dotenv configuration and improve CORS origin handling in app.js and index.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,16 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env"
+});
+
 import express from 'express';
 import cors from 'cors';
 
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || '*',
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
